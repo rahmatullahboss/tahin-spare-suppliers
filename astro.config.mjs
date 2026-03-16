faa9e42932c2301c424d51b24276380f004125f2
rahmatullahboss <rahmatullahzisan01@gmail.com>
2026-03-17 00:42:39 +0600

    speed

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,3 +1,24 @@
-import { defineConfig } from "astro/config";
+import { defineConfig, fontProviders } from "astro/config";
 
-export default defineConfig({});
+export default defineConfig({
+  build: {
+    inlineStylesheets: "always",
+  },
+  fonts: [
+    {
+      provider: fontProviders.google(),
+      name: "Inter",
+      cssVariable: "--font-inter",
+    },
+    {
+      provider: fontProviders.google(),
+      name: "Playfair Display",
+      cssVariable: "--font-playfair",
+    },
+    {
+      provider: fontProviders.google(),
+      name: "Dancing Script",
+      cssVariable: "--font-dancing",
+    },
+  ],
+});
